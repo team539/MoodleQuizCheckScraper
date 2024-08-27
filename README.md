d561fbe703c6f5c5aae617a2b3ec5e4427c2a8f5
Saburo Higuchi <hig@mail.ryukoku.ac.jp>
2024-08-27 11:55:25 +0900

    Update checkscraper.py Add fixed columns to represent the course, the question, a student and an attemp Add a head line

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # MoodleQuizCheckScraper
 
-* 教師が受験一覧のページを開く．
-* DownThemAllなどで， 全ユーザの review.html をローカルにダウンロードする
-* `python checkscraper.py`
+* 教師が受験結果のページ $(MOODLE)/mod/quiz/report.php?id=$(qid)&mode=overview を開く．
+  * 希望のソートをする．その順に review.html に番号がつく
+* [DownThemAll](https://chromewebstore.google.com/detail/downthemall/nljkibfhlpcnanjgbnlnbjecgicbjkge?hl=ja&pli=1)などで， 全ユーザの review.html をローカルにダウンロードする
+  * 個数だけクリックしなきゃいけない？
+
+### 実行
+
+```shell
+cat review.html | python3 checkscraper.py > review.csv
+```
+
+### コマンドラインオプション
+`--noheading`,`-nh` 先頭のヘッディングを省く
--- checkscraper.py
+++ checkscraper.py
@@ -2,41 +2,70 @@ import sys
 from bs4 import BeautifulSoup
 import csv
 import re
+import argparse
 
-html_content = sys.stdin.read()
+def main():
+    html_content = sys.stdin.read()
+    writer = csv.writer(sys.stdout)
 
-writer = csv.writer(sys.stdout)
-
-soup = BeautifulSoup(html_content, 'html.parser')
-print(soup.title.text)
-
-tables = soup.find_all('table',class_='generaltable')
-
-table = tables[0]
-row = table.find('tr')
-cols = row.find_all('td')  # Use 'th' if you want to extract headers
-#cols = [ele.text.strip() for ele in cols]
-print(cols[0].text)
-anchor=cols[0].find('a')
-url=anchor.get('href')
-# print(url)
-match=re.search(r'\?id=(\d+)\&',url)
-if match:
-    uid=match.group(1)
-    print(uid)
-
-table = tables[1]
-if True:
-    rows = table.find_all('tr')
-    data = []
-
-    for row in rows:
-        cols = row.find_all('td')  # Use 'th' if you want to extract headers
-        cols = [ele.text.strip() for ele in cols]
-        data.append(cols)
+    parser=argparse.ArgumentParser()
+    parser.add_argument("-nh","--noheading",action="store_true")
+    args=parser.parse_args()
     
-    for row in data:
-#        print(row)
-        writer.writerow(row)
+    soup = BeautifulSoup(html_content, 'html.parser')
+    title=soup.h1.text
+
+    tables = soup.find_all('table',class_='generaltable')
+
+    table = tables[0]
+    row = table.find('tr')
+    cols = row.find_all('td')  # Use 'th' if you want to extract headers
+
+    userdisplayname=cols[0].text
+    anchor=cols[0].find('a')
+    url=anchor.get('href')
+
+    match=re.search(r'\?id=(\d+)\&',url)
+    if match:
+        uid=match.group(1)
+
+    attemptid = soup.find('input', {'name': 'pageurl'})['value'].split('attempt=')[1].split('&')[0]
+    cmid = soup.find('input', {'name': 'pageurl'})['value'].split('cmid=')[1]
+
+
+    link = soup.find('a', href=lambda x: x and '/question/type/stack/questiontestrun.php' in x)
+
+    if link:
+        qid = link['href'].split('questionid=')[1].split('&')[0]
+
+    breadcrumb = soup.find(id='page-navbar')
+    cname = breadcrumb.find('a').text
+    cid = breadcrumb.find('a')['href'].split('id=')[1]
+
+    fixed=[cid,cname,qid,title,attemptid,uid,userdisplayname]
+    head=['cid','cname','qid','qname','attemptid','uid','uname','checkid','date','submission','status','grade']
     
+    table = tables[1]
+    if True:
+        rows = table.find_all('tr')
+        data = []
+
+    
+        for row in rows:
+            record = row.find_all('td')  # Use 'th' if you want to extract headers
+            record = [ele.text.strip() for ele in record]
+            if record:
+                data.append(fixed+record)
+
+            #TODO: decompse ansn etc
+            
+        if not args.noheading:
+            writer.writerow(head)
+
+        for row in data:
+            writer.writerow(row)
+        
 #    <div role="main"><table class="generaltable generalbox quizreviewsummary">
+
+if __name__ == "__main__":
+    main()
