710825e1a0773b28ca65cee99880bd08a467310b
Saburo Higuchi <hig@mail.ryukoku.ac.jp>
2024-10-14 10:42:18 +0900

    Add multiple-question support

--- checkscraper.py
+++ checkscraper.py
@@ -22,10 +22,9 @@ def main():
     soup = BeautifulSoup(html_content, 'html.parser')
     title=soup.h1.text
 
-    tables = soup.find_all('table',class_='generaltable')
+    reviewtable = soup.find('table',class_=['generaltable','quizreviewsummary'])
 
-    table = tables[0]
-    row = table.find('tr')
+    row = reviewtable.find('tr')
     cols = row.find_all('td')  # Use 'th' if you want to extract headers
 
     userdisplayname=cols[0].text
@@ -49,55 +48,65 @@ def main():
     else:
         cmid=0
         
-    link = soup.find('a', href=lambda x: x and '/question/type/stack/questiontestrun.php' in x)
-
-    if link:
-        qid = link['href'].split('questionid=')[1].split('&')[0]
-    else:
-        qid = 0
+    links = soup.find_all('a', href=lambda x: x and '/question/type/stack/questiontestrun.php' in x)
+    qids=[link['href'].split('questionid=')[1].split('&')[0] for link in links]
+#    for link in links:
+#    if link:
+#        qid = link['href'].split('questionid=')[1].split('&')[0]
+#    else:
+#        qid = 0
         
     breadcrumb = soup.find(id='page-navbar')
     cname = breadcrumb.find('a').text.replace(" ", "").replace("\n", "").replace("\t", "")
     cid = breadcrumb.find('a')['href'].split('id=')[1]
 
-    fixed=[cid,cname,qid,cmid,title,attemptid,uid,userdisplayname]
-    head=['cid','cname','qid','cmid','qname','attemptid','uid','uname','stepid','date','submission','status','grade']
+    questionid=0
+    fixed=[cid,cname,cmid,title,attemptid,uid,userdisplayname]
+    head=['cid','cname','cmid','qname','attemptid','uid','uname','qid','questionid','stepid','date','submission','status','grade']
     
-    table = tables[1]
-    if True:
-        rows = table.find_all('tr')
-        data = []
+    if not args.noheading:
+        writer.writerow(head+ansconfig.anshead+ansconfig.prthead)
     
-        for row in rows:
-            record = row.find_all('td')  # Use 'th' if you want to extract headers
-            record = [ele.text.strip() for ele in record]
-            if record:
-                submission=record[2].replace("\"\"","EMPTYSTRING") # bs4 interpretes &quot;&quot; as ""
-                record[2]=submission
-                
-                pattern = r'(ans\d+): ([^;]+) \[score\]' # to be strict, pattern should be read from ansconfig.anshead
-                matches = re.findall(pattern, submission) 
+    tables = soup.find_all('table',class_='generaltable')
+    
+    for table in tables:
+        if 'quizreviewsummary' not in table.get('class'):
+            questionid+=1 # should be retrieved from q55257:2_ans etc.
+            rows = table.find_all('tr')
+            data = []
+        
+            for row in rows:
+                record = row.find_all('td')  # Use 'th' if you want to extract headers
+                record = [ele.text.strip() for ele in record]
+                if record:
+                    submission=record[2].replace("\"\"","EMPTYSTRING") # bs4 interpretes &quot;&quot; as ""
+                    record[2]=submission
+                    
+                    pattern = r'(ans\d+): ([^;]+) \[score\]' # to be strict, pattern should be read from ansconfig.anshead
+                    matches = re.findall(pattern, submission) 
 
-                anslist=[]
-                for ans in ansconfig.anshead:
-                    result = next((tup[1] for tup in matches if tup[0] == ans),"")
-                    anslist.append(result)
+                    anslist=[]
+                    for ans in ansconfig.anshead:
+                        result = next((tup[1] for tup in matches if tup[0] == ans),"")
+                        anslist.append(result)
 
-                
-                pattern = r'(prt\d+)-(\d+-[TF])'
-                matches = re.findall(pattern, submission) # from submission
+                    
+                    pattern = r'(prt\d+)-(\d+-[TF])'
+                    matches = re.findall(pattern, submission) # from submission
 
-                prtlist=[]
-                for prt in ansconfig.prthead:
-                    result = [tup[1] for tup in matches if tup[0] == prt]
-                    prtlist.append(', '.join(map(str, result)))
-                data.append(fixed+record+anslist+prtlist)
-                
-        if not args.noheading:
-            writer.writerow(head+ansconfig.anshead+ansconfig.prthead)
+                    prtlist=[]
+                    for prt in ansconfig.prthead:
+                        result = [tup[1] for tup in matches if tup[0] == prt]
+                        prtlist.append(', '.join(map(str, result)))
+                    
+                    qid=0
+                    if len(qids)>=questionid:
+                        qid=qids[questionid-1]                        
+                    
+                    data.append(fixed+[qid,questionid]+record+anslist+prtlist)
 
-        for row in data:
-            writer.writerow(row)
+            for row in data:
+                writer.writerow(row)
         
 #    <div role="main"><table class="generaltable generalbox quizreviewsummary">
 
